09fd1d1f19583e2e5fbfa5fb06c5366a5e509012
AdarshLath <79131006+AdarshLath@users.noreply.github.com>
2021-10-13 16:01:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var first_name = "Adarsh" ;
+var second_name= "Lath";
+var full_name = first_name.concat(second_name);
+console.log(full_name);
+
+var fn = 15;
+var sn = 6;
+var sum = fn + sn;
+console.log(sum);
+
+var difference = fn - sn;
+console.log(difference);
+
+var product = fn*sn;
+console.log(product);
+
+var quotient = fn/sn;
+console.log(quotient); 
+
+var max_number = Math.max(fn,sn);
+console.log(max_number);
+
+var min_number = Math.min(fn,sn);
+console.log(min_number);
+
+var square_root = Math.sqrt(15);
+console.log(square_root);
+
+var power_eg_1 = Math.pow(fn,sn);
+console.log(power_eg_1);
